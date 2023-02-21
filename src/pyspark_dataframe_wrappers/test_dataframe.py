e2f1321779c85fc48529360dbb9b60ddb865421d
jmasonlee <jmasonlee@gmail.com>
2023-02-21 20:19:07 +0000

    B!!  fix pyright validation issue

--- src/pyspark_dataframe_wrappers/test_dataframe.py
+++ src/pyspark_dataframe_wrappers/test_dataframe.py
@@ -19,7 +19,7 @@ class TestDataFrame:
     def __init__(self, spark):
         self.spark = spark
         self.data = [{}]
-        self.explicit_schema: Optional[StructType] = StructType([])
+        self.explicit_schema: StructType = StructType([])
         self.fields = []
         self.base_data = {}
 
